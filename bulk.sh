6f4c5fd61358350e3dc54c354665583c3676f27f
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-06-07 09:21:08 -1000

    Changed bulk download script to accept arguments

--- bulk.sh
+++ bulk.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
+DATE=$1
+HOUR=$2
+MIN=$3
+CNT=$4
 
-for i in `seq -f "%02g" 0 5`;
+if [ "$CNT" == "" ]
+then
+  CNT=1
+fi
+
+if [ "$DATE" == "" -o "$HOUR" == "" -o "$MIN" == "" ]
+then
+  echo ""
+  echo " bulk.sh YYYY-MIN-DD hh mm [COUNT]"
+  echo ""
+  echo "   COUNT: [1-15]"
+  echo ""
+  exit 0
+fi
+
+while [ $CNT != 0 ]
 do
-  echo $i
-  node dist/index.js -s 2018-06-06T10:$i:00 -r 1m -o
+  HH=$(printf "%02d" $HOUR)
+  MM=$(printf "%02d" $MIN)
+
+  echo "node dist/index.js -s ${DATE}T${HH}:${MM}:00 -r 1m -o"
+  node dist/index.js -s ${DATE}T${HH}:${MM}:00 -r 1m -o
+
+  MIN=$(($MIN + 1))
+  if [ $MIN == 60 ]
+  then
+    MIN=0
+    HOUR=$(($HH + 1))
+  fi
+  if [ $HOUR == 24 ]
+  then
+    HOUR=0
+  fi
+  CNT=$(($CNT - 1))
 done 
