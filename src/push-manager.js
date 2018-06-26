a18d3b621a7de82a957b31bfe14aa69cc27366e1
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-06-26 12:02:41 -1000

    Fixed push to accept start time

--- src/push-manager.js
+++ src/push-manager.js
@@ -8,6 +8,9 @@ import Logger from './logger.js';
 
 let PushManager = {}
 
+const DEFAULT_API_ENDPOINT = 'http://localhost:9200/'
+const DEFAULT_INDEX = 'cflogs';
+
 /**
  * arg: { date, hour, archiveDir }
  */
@@ -21,9 +24,25 @@ PushManager.read = (arg) => {
   instream.pipe(linestream).pipe(converter).pipe(process.stdout);
 }
 
-//FIXME use config endpoint
+/**
+ * arg: { esApiEndpoint, index, file } 
+ *      file: path to a raw CloudFlare log json file
+ */
 PushManager.push = (arg) => {
-  let url = 'http://localhost:9200/_bulk'
+  let url = arg.esApiEndpoint || DEFAULT_API_ENDPOINT; 
+  url += '_bulk';
+  let index = arg.index || arg.IndexNameCurrent || DEFAULT_INDEX;
+
+  if (!fs.existsSync(arg.file)) {
+    Logger.error('PushManager#push File Not Found: ' + arg.file);
+    return;
+  } else {
+    let stat = fs.statSync(arg.file);
+    if (stat.size === 0) {
+      Logger.info('PushManager#push Empty File: ' + arg.file);
+      return;
+    }
+  }
   let instream = fs.createReadStream(arg.file)
     .on('error', (err) => { Logger.error(err) });
   let linestream = new LineStream()
--- src/push.js
+++ src/push.js
@@ -3,21 +3,23 @@ import fs from 'fs';
 import MyUtils from './my-utils.js';
 import PushManager from './push-manager.js';
 
+const WAIT_MINS = 20;
+
 const usage = () => {
   console.log("\n Usage: npm run push -- [OPTIONS]");
   console.log(" Usage: node dist/push.js [OPTIONS]");
   console.log("\n   OPTIONS");
   console.log("     -e or --env:        development|production default development");
-  console.log("     --date:             local time YYYY-MM-DD");
-  console.log("     --hour:             0-23");
+  console.log("     -s or --starttime:  local time YYYY-MM-DDThh:mm:ss default " + WAIT_MINS + " minutes before now");
+  console.log("     -i or --index:      index to push log data");
   console.log("     -h or --help:       show this message");
   console.log();
 };
 
 var opt = {
   env: 'development',
-  date: MyUtils.parseDate('2018-06-06'),
-  hour: 10
+  startTime: MyUtils.getTimeXminAgo(WAIT_MINS),
+  duration: 60000,
 };
 
 var args = process.argv.slice(2);
@@ -34,12 +36,11 @@ while(args.length > 0) {
     exitProgram();
   } else if (arg === '-e' || arg === '--env') {
     opt.env = args.shift();
-  } else if (arg === '--date') {
-    opt.originalDate = args.shift();
-    opt.date = MyUtils.parseDate(opt.originalDate);
-  } else if (arg === '--hour') {
-    opt.originalHour = args.shift();
-    opt.hour = parseInt(opt.originalHour);
+  } else if (arg === '-s' || arg === '--starttime') {
+    opt.originalStartTime = args.shift();
+    opt.startTime = MyUtils.parseTime(opt.originalStartTime);
+  } else if (arg === '-i' || arg === '--index') {
+    opt.index = args.shift();
   } else {
     exitProgram('Invalid Argument: ' + arg);
   }
@@ -49,12 +50,8 @@ if (opt.env != 'development' && opt.env != 'production') {
   exitProgram(" Invalid Environment: " + opt.env);
 }
 
-if (!opt.date) {
-  exitProgram(" Invalid Date: " + opt.originalDate);
-}
-
-if (!opt.hour || opt.hour < 0 || opt.hour > 23) {
-  exitProgram(" Invalid Hour: " + opt.originalHour);
+if (!opt.startTime) {
+  exitProgram(" Invalid Start Time: " + opt.originalStartTime);
 }
 
 let conf = MyUtils.initApp(opt);
@@ -62,6 +59,8 @@ if (!conf) {
   exitProgram('Configuration File Not Found: ' + MyUtils.config(opt));
 }
 
-//FIXME use passed arguments
-PushManager.push({ index: 'cflogs', file: './output/20180620/log.20180620012002-20180620012102.json' });
+conf.file = MyUtils.getLogFileName(conf);
+
+//PushManager.push({ index: 'cflogs', file: './output/20180620/log.20180620012002-20180620012102.json' });
+PushManager.push(conf);
 
