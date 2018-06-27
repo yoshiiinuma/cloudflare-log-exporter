02bec3c1692387fa11f1abcce38c5d95fc7b8e27
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-06-26 15:37:47 -1000

    Changed EsClient to accept an Index source path from the configuration file

--- src/es-client.js
+++ src/es-client.js
@@ -1,3 +1,5 @@
+
+import fs from 'fs';
 import request from 'request-promise';
 import util from 'util';
 import MyUtils from './my-utils.js';
@@ -8,10 +10,13 @@ const DEFAULT_INDEX_MAX_AGE = '10d';
 
 let EsClient = {};
 
-let endpoint = DEFAULT_ENDPOINT;
 const tempType = '_doc';
 const pretty = '?pretty';
 const v = '?v';
+
+const endpoint = (arg) => {
+  return arg.esApiEndpoint || DEFAULT_ENDPOINT;
+};
 
 let postProcess = (r) => {
   console.log(util.inspect(r, false, null));
@@ -26,106 +31,123 @@ const esGet = (url, data) => {
 
 const esPost = (url, data) => {
   let method = 'POST';
-  return request.post({ url, method, json: true, body: data })
+  return request({ url, method, json: true, body: data })
     .then(postProcess)
     .catch((err) => { Logger.error(err); });
 }
 
 const esPut = (url, data) => {
   let method = 'PUT';
-  console.log(url);
-  console.log(data);
-  return request.put({ url, method, json: true, body: data })
+  return request({ url, method, json: true, body: data })
     .then(postProcess)
     .catch((err) => { Logger.error(err); });
 }
 
 const esDel = (url) => {
   let method = 'DELETE';
-  return request.delete({ url, method, json: true })
+  return request({ url, method, json: true })
     .then(postProcess)
     .catch((err) => { Logger.error(err); });
 }
 
 EsClient.getHealth = (arg) => {
-  let url = endpoint + '_cat/health' + v;
+  let url = endpoint(arg) + '_cat/health' + v;
   return esGet(url);
 };
 
 EsClient.getNodes = (arg) => {
-  let url = endpoint + '_cat/nodes' + v;
+  let url = endpoint(arg) + '_cat/nodes' + v;
   return esGet(url);
 };
 
 EsClient.getTemplate = (arg) => {
-  let url = endpoint + '_template' + pretty;
+  let url = endpoint(arg) + '_template' + pretty;
   return esGet(url);
 };
 
 EsClient.getIndices = (arg) => {
-  let url = endpoint + '_cat/indices' + v;
+  let url = endpoint(arg) + '_cat/indices' + v;
   return esGet(url);
 };
 
 EsClient.getIndex = (arg) => {
-  let url = endpoint + arg.index + pretty;
-  console.log(url);
+  let url = endpoint(arg) + arg.index + pretty;
   return esGet(url);
 };
 
-//FIXME Do NOT use the hard-coded file name
+const getSrcData = (fpath) => {
+  if (!fpath) {
+    Logger.error('EsClient#getSrcData File Not Specified: ' + fpath);
+    return null;
+  } else if (!fs.existsSync(fpath)) {
+    Logger.error('EsClient#getSrcData File Not Found: ' + fpath);
+    return null;
+  }
+  return MyUtils.jsonToObject(fpath);
+};
+
 EsClient.putIndex = (arg) => {
-  let url = endpoint + arg.index + pretty;
-  let data = MyUtils.jsonToObject('./config/index.json');
+  let url = endpoint(arg) + arg.index + pretty;
+  let data = getSrcData(arg.esIndexSrc);
+  if (!data) {
+    Logger.error('EsClient#getSrcData Failed');
+    return Promise.resolve();
+  }
   return esPut(url, data);
 }
 
 EsClient.deleteIndex = (arg) => {
-  let url = endpoint + arg.index + pretty;
+  let url = endpoint(arg) + arg.index + pretty;
   return esDel(url);
 }
 
 EsClient.getMapping = (arg) => {
-  let url = endpoint + arg.index + '/_mapping/' + tempType + pretty;
+  let url = endpoint(arg) + arg.index + '/_mapping/' + tempType + pretty;
   return esGet(url);
 }
 
-//FIXME Do NOT use the hard-coded file name
 EsClient.putMapping = (arg) => {
-  let url = endpoint + arg.index + '/_mapping/' + tempType;
-  let data = MyUtils.jsonToObject('./config/mapping.json');
+  let url = endpoint(arg) + arg.index + '/_mapping/' + tempType;
+  let data = getSrcData(arg.esIndexMappingSrc);
+  if (!data) {
+    Logger.error('EsClient#getSrcData Failed');
+    return Promise.resolve();
+  }
   return esPut(url, data);
 }
 
 EsClient.putLog = (arg) => {
-  let url = endpoint + arg.index + '/' + tempType + pretty;
+  let url = endpoint(arg) + arg.index + '/' + tempType + pretty;
   return esPut(url);
 }
 
 EsClient.deleteLog = (arg) => {
-  let url = endpoint + arg.index + '/' + tempType + pretty;
+  let url = endpoint(arg) + arg.index + '/' + tempType + pretty;
   return esDel(url);
 }
 
 EsClient.bulkInsert = (arg) => {
-  let url = endpoint + '_bulk' + pretty;
+  let url = endpoint(arg) + '_bulk' + pretty;
   return esPost(url);
 };
 
 EsClient.getTemplate = (arg) => {
-  let url = endpoint + '_template/' + arg.index + pretty;
+  let url = endpoint(arg) + '_template/' + arg.index + pretty;
   return esGet(url);
 };
 
-//FIXME Do NOT use the hard-coded file name
 EsClient.putTemplate = (arg) => {
-  let url = endpoint + '_template/' + arg.index + pretty;
-  let data = MyUtils.jsonToObject('./config/template.json');
+  let url = endpoint(arg) + '_template/' + arg.index + pretty;
+  let data = getSrcData(arg.esIndexTemplateSrc);
+  if (!data) {
+    Logger.error('EsClient#getSrcData Failed');
+    return Promise.resolve();
+  }
   return esPost(url, data);
 };
 
 EsClient.rollover = (arg) => {
-  let url = endpoint + arg.index + '/_rollover';
+  let url = endpoint(arg) + arg.index + '/_rollover';
   let maxAge = arg.maxAge || DEFAULT_INDEX_MAX_AGE;
   let cond = { conditions: {} };
 
@@ -139,21 +161,6 @@ EsClient.rollover = (arg) => {
     cond.conditions.max_size = arg.maxSize;
   }
   return esPost(url, cond);
-};
-
-const tempTemplate = {
-  "template": "cflogs-*",
-  "settings": {
-    "index": {
-      "number_of_shards": 5,
-      "number_of_replicas": 1,
-      "routing.allocation.include.box_type": "hot",
-      "routing.allocation.total_shards_per_node": 1
-    }
-  },
-  "aliases": {
-    "all-cflogs": {}
-  }
 };
 
 export default EsClient;
