8e6669d6b03ff68bf4e61bce21f1e2ca3c171d78
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-06-28 12:11:52 -1000

    Moved BulkInsertConverter message prefix creation to the constructor

--- src/bulk-insert-converter.js
+++ src/bulk-insert-converter.js
@@ -7,13 +7,13 @@ class BulkInsertConverter extends Transform {
   constructor(index = DEFAULT_INDEX, options = {}) {
     super(options);
     this.index = index;
+    this.action = "{\"index\":{\"_index\":\"" + this.index + "\",\"_type\":\"_doc\"}}\n";
   }
 
   _transform(chunk, encoding, callback) {
     let json = JSON.parse(chunk.toString());
     json['@timestamp'] = new Date(json.EdgeStartTimestamp/1000000);
-    let msg = "{\"index\":{\"_index\":\"" + this.index + "\",\"_type\":\"_doc\"}}\n";
-    msg += JSON.stringify(json) + "\n";
+    let msg = this.action + JSON.stringify(json) + "\n";
     this.push(msg);
     callback();
   }
