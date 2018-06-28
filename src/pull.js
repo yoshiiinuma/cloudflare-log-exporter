34e411e8e2e48832478f69ec86e39c91967b458e
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-06-28 08:36:01 -1000

    Improved pull.js log messages

--- src/pull.js
+++ src/pull.js
@@ -30,7 +30,7 @@ var opt = {
 var args = process.argv.slice(2);
 
 var exitProgram = (msg) => {
-  if (msg) console.log(msg);
+  if (msg) console.log('PULL.JS ' + msg);
   usage();
   process.exit();
 }
